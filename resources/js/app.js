5176b7dd9a573ddda6b9e14a412b6c9738d9b6de
Eric Anthony Wu <ericanthonywu89@gmail.com>
2019-08-13 11:28:11 +0700

    first init setting intellij idea && mdbreact init

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
+ */
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import "./assets/scss/mdb.scss"
+import "./assets/scss/_custom-styles.scss"
+
+require('./bootstrap');
+
+/**
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+require('./components/Example');
